14d2341e0fe34bce240c5e363ca33186af1cfc3f
erictw <eric.wohltman@gmail.com>
2017-12-26 16:27:18 -0500

    Improved handling of graceful shutdowns

--- ephemeral-roles.go
+++ ephemeral-roles.go
@@ -129,7 +129,9 @@ func main() {
 	dgBot.Close()
 
 	// Cleanly shutdown the HTTP server
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+
 	httpServer.Shutdown(ctx)
 
 	return
