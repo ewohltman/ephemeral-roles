3bdaef2ffde09b3e49fb1cb4f37a49fc4eb9f9df
Eric Wohltman <eric.wohltman@gmail.com>
2019-12-27 10:16:20 -0500

    Handle unhandled error and improve test coverage

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,18 +1,52 @@
 package server
 
 import (
+	"bytes"
+	"net/http"
 	"testing"
 
 	"github.com/ewohltman/ephemeral-roles/pkg/logging"
 	"github.com/sirupsen/logrus"
 )
 
+const testPort = "8080"
+
 func TestNew(t *testing.T) {
 	log := logging.New()
 	log.SetLevel(logrus.FatalLevel)
 
-	testServer := New(log, "8080")
+	testServer := New(log, testPort)
 	if testServer == nil {
-		t.Errorf("Failed creating new internal HTTP server")
+		t.Fatal("Unexpected nil *http.Server")
 	}
+
+	stopChan := make(chan struct{})
+
+	go func() {
+		err := testServer.ListenAndServe()
+		if err != nil {
+			t.Fatalf("Error starting test server: %s", err)
+		}
+
+		<-stopChan
+
+		err = testServer.Close()
+		if err != nil {
+			t.Errorf("Error closing test server: %s", err)
+		}
+	}()
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:"+testPort, bytes.NewReader([]byte{}))
+	if err != nil {
+		t.Fatalf("Error creating test request: %s", err)
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("Error performing test request: %s", err)
+	}
+
+	close(stopChan)
 }
