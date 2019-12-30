00204984be27cee538dedbbb1643289ae10bca5c
Eric Wohltman <eric.wohltman@gmail.com>
2019-12-30 11:17:40 -0500

    Update to show bot info if only bot keyword is provided

--- pkg/callbacks/messageCreate.go
+++ pkg/callbacks/messageCreate.go
@@ -4,13 +4,18 @@ import (
 	"os"
 	"strings"
 
+	"github.com/ewohltman/ephemeral-roles/pkg/environment"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/ewohltman/ephemeral-roles/pkg/logging"
 	"github.com/sirupsen/logrus"
 )
 
-const logoURL = "https://raw.githubusercontent.com/ewohltman/ephemeral-roles" +
-	"/master/web/static/logo_Testa_anatomica_(1854)_-_Filippo_Balbi.jpg"
+const (
+	logoURL = "https://raw.githubusercontent.com/ewohltman/ephemeral-roles" +
+		"/master/web/static/logo_Testa_anatomica_(1854)_-_Filippo_Balbi.jpg"
+	logLevelChange = "Logging level changed"
+)
 
 // MessageCreate is the callback function for the MessageCreate event from Discord
 func (config *Config) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -24,7 +29,7 @@ func (config *Config) MessageCreate(s *discordgo.Session, m *discordgo.MessageCr
 
 	// [BOT_KEYWORD] [command] [options] :: "!eph" "log_level" "debug"
 	contentTokens := strings.Split(strings.TrimSpace(m.Content), " ")
-	if len(contentTokens) < 2 {
+	if len(contentTokens) < 1 {
 		return
 	}
 
@@ -58,10 +63,61 @@ func (config *Config) MessageCreate(s *discordgo.Session, m *discordgo.MessageCr
 	config.parseMessage(s, m.ChannelID, contentTokens)
 }
 
+func (config *Config) parseMessage(s *discordgo.Session, channelID string, contentTokens []string) {
+	if len(contentTokens) < 2 {
+		config.handleInfo(s, channelID)
+		return
+	}
+
+	switch strings.ToLower(contentTokens[1]) {
+	case "info":
+		config.handleInfo(s, channelID)
+	case "log_level":
+		config.handleLogLevel(contentTokens)
+	default: // Do nothing for unrecognized command
+	}
+}
+
+func (config *Config) handleInfo(s *discordgo.Session, channelID string) {
+	_, err := s.ChannelMessageSendEmbed(channelID, infoMessage())
+	if err != nil {
+		config.Log.WithError(err).Debugf("Unable to send info message")
+	}
+}
+
+func (config *Config) handleLogLevel(contentTokens []string) {
+	if len(contentTokens) >= 3 {
+		levelOpt := strings.ToLower(contentTokens[2])
+
+		logFields := logrus.Fields{"log_level": levelOpt}
+
+		switch levelOpt {
+		case "debug":
+			config.updateLogLevel(levelOpt)
+			config.Log.WithFields(logFields).Debugf(logLevelChange)
+		case "info":
+			config.updateLogLevel(levelOpt)
+			config.Log.WithFields(logFields).Infof(logLevelChange)
+		case "warn":
+			config.updateLogLevel(levelOpt)
+			config.Log.WithFields(logFields).Warnf(logLevelChange)
+		case "error":
+			config.updateLogLevel(levelOpt)
+			config.Log.WithFields(logFields).Errorf(logLevelChange)
+		case "fatal":
+			config.updateLogLevel(levelOpt)
+		case "panic":
+			config.updateLogLevel(levelOpt)
+		}
+	}
+}
+
 func (config *Config) updateLogLevel(levelOpt string) {
 	err := os.Setenv("LOG_LEVEL", levelOpt)
 	if err != nil {
-		config.Log.WithError(err).Warn("Unable to set LOG_LEVEL environment variable")
+		config.Log.
+			WithError(err).
+			Warnf("Unable to set %s environment variable", environment.LogLevel)
 		return
 	}
 
@@ -82,56 +138,19 @@ func infoMessage() *discordgo.MessageEmbed {
 		Fields: []*discordgo.MessageEmbedField{
 			{
 				Name:   "About",
-				Value:  "Ephemeral Roles is a discord bot designed to assign roles based upon voice channel member presence",
+				Value:  "Ephemeral Roles is a Discord bot designed to assign roles based upon voice channel member presence.",
 				Inline: false,
 			},
 			{
 				Name:   "Author",
-				Value:  "Ephemeral Roles is created by ewohltman",
+				Value:  "Ephemeral Roles was created by ewohltman: https://github.com/ewohltman",
 				Inline: false,
 			},
 			{
 				Name:   "Library",
-				Value:  "Ephemeral Roles uses the discordgo library by bwmarrin",
+				Value:  "Ephemeral Roles uses the `discordgo` library by bwmarrin: https://github.com/bwmarrin/discordgo",
 				Inline: false,
 			},
 		},
-	}
-}
-
-func (config *Config) parseMessage(s *discordgo.Session, channelID string, contentTokens []string) {
-	switch strings.ToLower(contentTokens[1]) {
-	case "info":
-		_, err := s.ChannelMessageSendEmbed(channelID, infoMessage())
-		if err != nil {
-			config.Log.WithError(err).Debugf("Unable to send message")
-			return
-		}
-	case "log_level":
-		if len(contentTokens) >= 3 {
-			levelOpt := strings.ToLower(contentTokens[2])
-
-			logFields := logrus.Fields{"log_level": levelOpt}
-
-			switch levelOpt {
-			case "debug":
-				config.updateLogLevel(levelOpt)
-				config.Log.WithFields(logFields).Debugf("Logging level changed")
-			case "info":
-				config.updateLogLevel(levelOpt)
-				config.Log.WithFields(logFields).Infof("Logging level changed")
-			case "warn":
-				config.updateLogLevel(levelOpt)
-				config.Log.WithFields(logFields).Warnf("Logging level changed")
-			case "error":
-				config.updateLogLevel(levelOpt)
-				config.Log.WithFields(logFields).Errorf("Logging level changed")
-			case "fatal":
-				config.updateLogLevel(levelOpt)
-			case "panic":
-				config.updateLogLevel(levelOpt)
-			}
-		}
-	default: // Do nothing for unrecognized command
 	}
 }
